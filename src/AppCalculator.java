e4010f0b5130db9bf85906745af77a387b9833c8
sergewax <whitewaxbox@gmail.com>
2022-11-09 17:17:47 +0300

    commit: finish simple calculator

--- src/AppCalculator.java
+++ src/AppCalculator.java
@@ -12,14 +12,62 @@ public class AppCalculator extends JFrame {
         JButton minus = new JButton("-");
         JButton multiply = new JButton("*");
         JButton divide = new JButton("/");
+        JLabel res = new JLabel("result");
         JTextField x1 = new JTextField("1");
         JTextField x2 = new JTextField("2");
         ActionListener listenerPlus = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int x_1 = Integer.parseInt(x1.getText());
+                int x_2 = Integer.parseInt(x2.getText());
+                result = x_1 + x_2;
+                res.setText(String.valueOf(result));
             }
+        };
+        ActionListener listenerMinus = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                int x_1 = Integer.parseInt(x1.getText());
+                int x_2 = Integer.parseInt(x2.getText());
+                result = x_1 - x_2;
+                res.setText(String.valueOf(result));
+                }
+        };
+        ActionListener listenerMultiply = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int x_1 = Integer.parseInt(x1.getText());
+                int x_2 = Integer.parseInt(x2.getText());
+                result = x_1 * x_2;
+                res.setText(String.valueOf(result));
+            }
+        };
+        ActionListener listenerDivide = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int x_1 = Integer.parseInt(x1.getText());
+                int x_2 = Integer.parseInt(x2.getText());
+                result = x_1 / x_2;
+                res.setText(String.valueOf(result));
+            }
+        };
+        plus.addActionListener(listenerPlus);
+        minus.addActionListener(listenerMinus);
+        multiply.addActionListener(listenerMultiply);
+        divide.addActionListener(listenerDivide);
+        panel.setSize(new Dimension(500, 500));
+        add(panel);
+        panel.add(res);
+        panel.add(plus);
+        panel.add(minus);
+        panel.add(multiply);
+        panel.add(divide);
+        panel.add(x1);
+        panel.add(x2);
 
-        }
+        setPreferredSize(new Dimension(600,600));
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
     }
 }
